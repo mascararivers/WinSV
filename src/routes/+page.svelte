92fe100355b6e9d308f1d8c359fd326ecd64291f
max <hxllfxres@gmail.com>
2024-10-07 17:15:02 +0100

    added login screen and basic gsap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import '../scripts/gsap'
+</script>
+
+<img src="/background-1.jpg" class="w-screen h-screen absolute" alt="" id="login-background">
+<div class="flex flex-col justify-center items-center z-10 w-screen h-screen absolute gap-10">
+    <div class="w-64 h-64 bg-zinc-400 opacity-70 rounded-full flex flex-col gap-2 justify-center items-center border-4" id="user-field">
+        <img src="/user.png" alt="" class="w-32 h-32">
+        <input type="text" placeholder="Username" class="h-8 w-32 rounded-md text-center bg-zinc-600 placeholder-white text-white">
+    </div>
+    <button class="w-64 h-12 rounded-md bg-white opacity-50 text-xl hover:scale-110 duration-200" id="enter-btn">Enter</button>
+</div>
+
+<style>
+    :root {
+        background-color: black;
+    }
+</style>
---
+++ src/scripts/gsap.ts
@@ -0,0 +1,22 @@
+import { browser } from "$app/environment";
+import { gsap } from "gsap";
+
+if(browser) {
+    const tl = gsap.timeline({})
+
+    tl.set('#user-field', {
+        y: -100, opacity: 0
+    }).set('#enter-btn', {
+        y: 100, opacity: 0
+    }).set('#login-background', {
+        opacity: 0
+    })
+
+    tl.to('#user-field', {
+        y:0, opacity: 0.7, duration: 1, ease: 'expo.out'
+    }).to('#enter-btn', {
+        y: 0, opacity: 0.7, duration: 0.5, ease: 'expo.out'
+    }, 0).to('#login-background', {
+        opacity: 1
+    }, 0)
+}
