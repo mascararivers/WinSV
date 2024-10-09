4e67d892ea9ded6ff8074dab35c35efa865f9547
max <hxllfxres@gmail.com>
2024-10-09 01:26:13 +0100

    improved login screen

--- src/scripts/gsap.ts
+++ src/scripts/gsap.ts
@@ -1,7 +1,7 @@
 import { browser } from "$app/environment";
 import { gsap } from "gsap";
 
-if(browser) {
+if (browser) {
     const tl = gsap.timeline({})
 
     tl.set('#user-field', {
@@ -10,13 +10,21 @@ if(browser) {
         y: 100, opacity: 0
     }).set('#login-background', {
         opacity: 0
+    }).set('#username-field', {
+        x: -100, opacity: 0
+    }).set('#password-field', {
+        x: 100, opacity: 0
     })
 
     tl.to('#user-field', {
-        y:0, opacity: 0.7, duration: 1, ease: 'expo.out'
+        y: 0, opacity: 0.7, duration: 1, ease: 'expo.out'
     }).to('#enter-btn', {
         y: 0, opacity: 0.7, duration: 0.5, ease: 'expo.out'
     }, 0).to('#login-background', {
         opacity: 1
+    }, 0).to('#username-field', {
+        x: 0, opacity: 1
+    }, 0).to('#password-field', {
+        x: 0, opacity: 1
     }, 0)
 }
